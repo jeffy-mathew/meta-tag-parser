5cf3e91ef336ac4790b32e5a7df83007c78cbe9e
Jeffy <jeffy.mathew100@gmail.com>
2019-02-22 19:59:18 +0530

    added basic validation for port in use exception and url parameter value validations

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var fs = require('fs');
+// get configuration settings
+var obj = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
+
 app.use( bodyParser.json() );     
 app.use(bodyParser.urlencoded({     
   extended: true
 })); 
+
 //Routes
 app.use(require('./router'));
  
-var server = app.listen(8000, function () {
-  console.log("Server Listening on port 8000");
-})
+var server = app.listen(obj.port, function (err) {
+  console.log("Server Listening on port " + obj.port);
+});
+
+server.on("error",(err) =>{
+  // handle port in use exception
+  if(err.errno === 'EADDRINUSE')
+         console.log("port " + obj.port + " already in use");
+    else
+         console.log(err);
+  process.exit(1);
+});
--- routes/meta_parser.js
+++ routes/meta_parser.js
@@ -1,51 +1,58 @@
 var cheerio = require('cheerio');
 var request = require('request');
 var express = require('express');
+var validUrl = require('valid-url');
 var router = express.Router();
 
 router.post('/', function(req, res) {
-    var options = {
-        url: req.body.url,
-        timeout: 5000,
-        headers: {
-            'User-Agent': 'request'
-        }
-    };
-    request.get(options, function (error, response, body) {
-        if (!error && response.statusCode === 200) {
-            var bodyObj = cheerio.load(body);
-            var meta = bodyObj('meta');
-            var keys = Object.keys(meta);
-            var meta_obj = {};
-            var og_meta_obj = {};
-            keys.forEach(function (key) {
-                if (meta[key].attribs != undefined) {
-                    if (meta[key].attribs.property && meta[key].attribs.content) {
-                        if(meta[key].attribs.property.includes("og:")){
+    if(!req.body.url){
+        res.status(422).send("URL parameter is empty");
+    }else if(!validUrl.isUri(req.body.url)){
+        res.status(422).send("Invalid URL")
+    }else{
+        var options = {
+            url: req.body.url,
+            timeout: 5000,
+            headers: {
+                'User-Agent': 'request'
+            }
+        };
+        request.get(options, function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                var bodyObj = cheerio.load(body);
+                var meta = bodyObj('meta');
+                var keys = Object.keys(meta);
+                var meta_obj = {};
+                var og_meta_obj = {};
+                keys.forEach(function (key) {
+                    if (meta[key].attribs != undefined) {
+                        if (meta[key].attribs.property && meta[key].attribs.content) {
+                            if(meta[key].attribs.property.includes("og:")){
+                                // find if the tag is meta tag
+                                og_meta_obj[meta[key].attribs.property] = meta[key].attribs.content;
+                            }else{
+                                meta_obj[meta[key].attribs.property] = meta[key].attribs.content;
+                            }
+                        }else if (meta[key].attribs.name && meta[key].attribs.content) {
                             // find if the tag is meta tag
-                            og_meta_obj[meta[key].attribs.property] = meta[key].attribs.content;
-                        }else{
-                            meta_obj[meta[key].attribs.property] = meta[key].attribs.content;
-                        }
-                    }else if (meta[key].attribs.name && meta[key].attribs.content) {
-                        // find if the tag is meta tag
-                        if(meta[key].attribs.name.includes("og:")){
-                            og_meta_obj[meta[key].attribs.name] = meta[key].attribs.content;
-                        }else{
-                            meta_obj[meta[key].attribs.name] = meta[key].attribs.content;
+                            if(meta[key].attribs.name.includes("og:")){
+                                og_meta_obj[meta[key].attribs.name] = meta[key].attribs.content;
+                            }else{
+                                meta_obj[meta[key].attribs.name] = meta[key].attribs.content;
+                            }
                         }
                     }
-                }
-            });
-            res.send({ "meta_obj" : meta_obj, "og:data" : og_meta_obj});
-        } else {
-            if (response && typeof response.statusCode !== 'undefined') {
-                res.status(response.statusCode).send("URL not a success response");
+                });
+                res.send({ "meta_obj" : meta_obj, "og:data" : og_meta_obj});
             } else {
-                res.status(404).send("URL resource not found");
+                if (response && typeof response.statusCode !== 'undefined') {
+                    res.status(response.statusCode).send("URL not a success response");
+                } else {
+                    res.status(404).send("URL resource not found");
+                }
             }
-        }
-    });
+        });
+    }
 });
 
 module.exports.router = router;
