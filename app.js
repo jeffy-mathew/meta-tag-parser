f9464fcb4d649d73e05322a5194dad68ac5d7d79
Jeffy <jeffy.mathew100@gmail.com>
2019-02-22 19:32:18 +0530

    created server in app.js router and corresponsing get_meta_data route

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser')
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+app.use( bodyParser.json() );     
+app.use(bodyParser.urlencoded({     
   extended: true
 })); 
 //Routes
